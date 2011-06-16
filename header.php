8853224b211aac29c71961ab1a318a2b8cbdf99f
the Wizard of z/OS <wizard@haakaa.nl>
2011-06-16 20:25:05 +0200

    woot first post

---
+++ header.php
@@ -0,0 +1,5 @@
+<!DOCTYPE html PUBLIC "-//W3C//DDT XHTML 1.0 Strinct/EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"> <!-- that old? -->
+<meta http-equiv="Content-Type" content="text/html" content="text/html; charset=UTF=8" />
+
+<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
